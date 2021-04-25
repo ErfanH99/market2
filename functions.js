c2209e746edd600b19266d109ca2812cb64e42d8
ErfanH99 <erfan.hero.7@gmail.com>
2021-04-25 16:12:35 -0700

    first commit

---
+++ functions.js
@@ -0,0 +1,81 @@
+function focusinput(){
+    alert("Please Enter Your Username !")
+}
+
+function blurinput() {
+    alert("Are You Sure ?") 
+}
+
+function keyupinput(event) {
+    if (event.keyCode != 8 && event.keyCode >= 65 && event.keyCode <= 90 || event.keyCode >= 32){
+        document.getElementById('counter').innerHTML = eval(document.getElementById('counter').innerHTML) + 1
+    }
+    else if (event.keyCode == 8 && eval(document.getElementById('counter').innerHTML) != 0){
+        document.getElementById('counter').innerHTML = eval(document.getElementById('counter').innerHTML) - 1
+    }
+    
+}
+
+function keydowninput() {
+    alert("keydown !!")
+}
+
+function mouseover(elm){
+    document.getElementById('hoverme_txt').innerHTML="sign up for free"
+    elm.style.color="yellow"
+    elm.style.borderRadius="20%"
+}
+function mouseout(elm){
+    document.getElementById('hoverme_txt').innerHTML = "hover me"
+    elm.style.color="navy"
+    elm.style.borderRadius="50%"
+}
+
+function txtclickinput() {
+    alert("Write about yourself !")
+}
+
+function signupblur() {
+    alert("You'r Account has been created.")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mouseoverimg(elm) {
+    elm.src = "./files/s21-2.jpg"
+}
+function mouseoutimg(elm) {
+    elm.src = "./files/s21.jpg"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
